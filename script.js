7809d40c1c25346096022bac17ab0e3c7a339955
ciut1st <williamyeoshuhok1st@gmail.com>
2023-08-16 12:45:54 +1000

    done for now

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead()
+    render()
+}
+
 function addBookToLibrary() {
     let title = document.querySelector('#title').value
     let author = document.querySelector('#author').value
@@ -29,5 +38,27 @@ function addBookToLibrary() {
 }
 
 function render() {
-    let libraryBook = document.querySelector('.library')
+    let libraryDiv = document.querySelector('.library')
+    libraryDiv.innerHTML = ""
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i]
+        let bookDiv = document.createElement("div")
+        bookDiv.innerHTML = `
+        <div class="card-header">
+            <h3 class="title">${book.title}</h3>
+            <h5 class="author">by ${book.author}</h5>
+        </div>
+        <div class="card-body">
+            <p>${book.pages} pages</p>
+            <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+            <button class="remove-button" onclick="removeBook(${i})">Remove</button>
+            <button class="toggle-read-button" onclick="toggleRead(${i})">Toggle Read</button>
+        `
+        libraryDiv.appendChild(bookDiv)
+    }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1)
+    render()
 }
